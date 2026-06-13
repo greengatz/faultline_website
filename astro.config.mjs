c76c82e55dd37ee9e1c5b0fda6b76d39faffd9d2
root <root@LAPTOP-PVA5G8A5.localdomain>
2026-06-13 10:18:10 -0700

    add cname for faultline40k.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://greengatz.github.io',
-  base: '/faultline_website',
+  site: 'https://www.faultline40k.com',
 });
